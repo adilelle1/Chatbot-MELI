ef34e958316a21c5636c1c3fd7b1d5744292887f
adilelle1 <106268116+adilelle1@users.noreply.github.com>
2023-10-26 15:22:56 -0300

    cambio titulo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ from hugchat import hugchat
 from hugchat.login import Login
 
 # App title
-st.set_page_config(page_title=f"{''':handshake:'''}Chatbot Meli{''':handshake:'''}")
+st.set_page_config(page_title=f"{''':handshake:'''} Chatbot Meli {''':handshake:'''}")
 
 # Hugging Face Credentials
 with st.sidebar:
-    st.title(f"{''':handshake:'''}Chatbot Meli{''':handshake:'''}")
+    st.title(f"{''':handshake:'''} Chatbot Meli {''':handshake:'''}")
     if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
         st.success('Ya recibimos tus credenciales de HuggingFace!', icon='✅')
         hf_email = st.secrets['EMAIL']
